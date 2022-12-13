b01361e7ae73e027d37040c34a1060ce8bc2cad3
RovarisTales <tales.rovaris@hotmail.com>
2022-12-13 09:09:21 +0000

    base de dados ai pra todo mundo

---
+++ amazoniabd/amazonia.sql
@@ -0,0 +1,115 @@
+use Amazonia;
+alter table Funcionario add column data_nascimento date;
+CREATE TABLE Pedido (
+    ID_Pedido INTEGER PRIMARY KEY,
+    Endereco_Entrega VARCHAR(45),
+    Cupao VARCHAR(9),
+    Estado VARCHAR(1),
+    Total FLOAT,
+    Data_Entrega DATE,
+    Data_Pedido DATE,
+    Data_Envio DATE,
+    fk_Cliente_ID_Cliente INTEGER,
+    UNIQUE (ID_Pedido, fk_Cliente_ID_Cliente)
+);
+
+alter table Cliente add column Genero varchar(1);
+CREATE TABLE Cliente (
+    ID_Cliente INTEGER PRIMARY KEY,
+    Nome VARCHAR(45),
+    Email VARCHAR(45),
+    Telemovel INTEGER,
+    Metodos_Pagamento VARCHAR(45),
+    Rua VARCHAR(45),
+    Cod_Postal INTEGER
+);
+
+CREATE TABLE Item (
+    Item_ID INTEGER PRIMARY KEY UNIQUE,
+    Preco FLOAT,
+    Tipo VARCHAR(45),
+    Descricao VARCHAR(45)
+);
+alter table Item add column Tipo varchar(45);
+CREATE TABLE Funcionario (
+    Funcionario_ID INTEGER PRIMARY KEY,
+    Nome VARCHAR(45),
+    Tipo CHAR,
+    Salario FLOAT,
+    Endereco VARCHAR(45),
+    Desempenho INTEGER,
+    fk_Armazem_Armazem_ID INTEGER,
+    UNIQUE (Funcionario_ID, fk_Armazem_Armazem_ID)
+);
+
+CREATE TABLE Armazem (
+    Armazem_ID INTEGER PRIMARY KEY UNIQUE,
+    Nome VARCHAR(45),
+    Localidade VARCHAR(45)
+);
+
+CREATE TABLE tem (
+    fk_Funcionario_Funcionario_ID INTEGER,
+    fk_Pedido_ID_Pedido INTEGER,
+    PRIMARY KEY (fk_Funcionario_Funcionario_ID, fk_Pedido_ID_Pedido),
+    UNIQUE (fk_Funcionario_Funcionario_ID, fk_Pedido_ID_Pedido)
+);
+
+CREATE TABLE contem (
+    fk_Item_Item_ID INTEGER,
+    fk_Pedido_ID_Pedido INTEGER,
+    Quantidade INTEGER,
+    Valor FLOAT,
+    IVA INTEGER,
+    PRIMARY KEY (fk_Item_Item_ID, fk_Pedido_ID_Pedido),
+    UNIQUE (fk_Item_Item_ID, fk_Pedido_ID_Pedido)
+);
+
+CREATE TABLE possui (
+	pos varchar(4),
+    stock int,
+    fk_Armazem_Armazem_ID INTEGER,
+    fk_Item_Item_ID INTEGER,
+    PRIMARY KEY (fk_Armazem_Armazem_ID, fk_Item_Item_ID),
+    UNIQUE (fk_Armazem_Armazem_ID, fk_Item_Item_ID)
+);
+ 
+ALTER TABLE Pedido ADD CONSTRAINT FK_Pedido_2
+    FOREIGN KEY (fk_Cliente_ID_Cliente)
+    REFERENCES Cliente (ID_Cliente)
+    ON DELETE CASCADE;
+ 
+ALTER TABLE Funcionario ADD CONSTRAINT FK_Funcionario_2
+    FOREIGN KEY (fk_Armazem_Armazem_ID)
+    REFERENCES Armazem (Armazem_ID)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE tem ADD CONSTRAINT FK_tem_1
+    FOREIGN KEY (fk_Funcionario_Funcionario_ID)
+    REFERENCES Funcionario (Funcionario_ID)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE tem ADD CONSTRAINT FK_tem_2
+    FOREIGN KEY (fk_Pedido_ID_Pedido)
+    REFERENCES Pedido (ID_Pedido)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE contem ADD CONSTRAINT FK_contem_1
+    FOREIGN KEY (fk_Item_Item_ID)
+    REFERENCES Item (Item_ID)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE contem ADD CONSTRAINT FK_contem_2
+    FOREIGN KEY (fk_Pedido_ID_Pedido)
+    REFERENCES Pedido (ID_Pedido)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE possui ADD CONSTRAINT FK_possui_1
+    FOREIGN KEY (fk_Armazem_Armazem_ID)
+    REFERENCES Armazem (Armazem_ID)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE possui ADD CONSTRAINT FK_possui_2
+    FOREIGN KEY (fk_Item_Item_ID)
+    REFERENCES Item (Item_ID)
+    ON DELETE RESTRICT;
---
+++ amazoniabd/queries.sql
@@ -0,0 +1,86 @@
+use Amazonia;
+
+
+
+
+
+select Funcionario_ID from Funcionario where Funcionario_ID = fk_Funcionario_Funcionario_ID;
+
+Delimiter &&
+CREATE PROCEDURE `PedFunc` (IN id_ped INT)
+BEGIN
+SELECT Nome FROM Funcionario where fk_Pedido_ID_Pedido = id_ped;
+END &&
+delimiter ;
+
+
+select Nome from Funcionario order by desempenho desc;
+
+select count(Descricao),fk_Item_Item_ID from Item, contem where Item_ID = fk_Item_Item_ID;
+
+
+
+
+
+create view top_gastos as
+select fk_Cliente_ID_Cliente, sum(total) as gasto
+from Pedido
+group by fk_Cliente_ID_Cliente;
+drop view precoV;
+create view precoV as
+select fk_Pedido_ID_Pedido,v*c as gasto
+w
+from contem
+where fk_Pedido_ID_Pedido = 35
+group by fk_Pedido_ID_Pedido;
+
+select * from precoV;
+
+select * from Pedido;
+
+select * from top_gastos;
+
+delimiter &&
+create procedure calc_total()
+begin
+declare x int;
+set x = 1;
+banana:
+while x < 38 do
+update Pedido set total = (select sum(quantidade*valor) as val from contem where fk_Pedido_ID_Pedido = x) where ID_Pedido = x;
+set x = x+1;
+end while
+banana;
+end &&
+delimiter ;
+
+select * from Pedido;
+
+
+select quantidade as q ,valor as v from contem where fk_Pedido_ID_Pedido = 1;
+SELECT *
+FROM possui
+Group By fk_Armazem_Armazem_ID
+ORDER BY fk_Armazem_Armazem_ID
+delimiter ;
+
+
+select quantidade as q ,valor as v ,fk_Pedido_ID_Pedido as ped from contem where fk_Pedido_ID_Pedido = x group by pedido;
+
+update pedido set valor = q*v where Pedido_ID = ped where;
+select quantidade as q from contem where fk_Pedido_ID_Pedido = 34;
+
+
+select Funcionario_ID, Salario/Desempenho as Quota from Funcionario;
+
+
+update Pedido set total = (select sum(quantidade*valor) as val from contem where fk_Pedido_ID_Pedido = 1) where ID_Pedido = 1;
+
+select total from Pedido;
+
+select sum(quantidade*valor) as val from contem where fk_Pedido_ID_Pedido = 1;
+
+update Pedido set total = (select sum(quantidade*valor) as val from contem where fk_Pedido_ID_Pedido = 32) where ID_Pedido = 32;
+
+
+select Desempenho from Funcionario;
