d9a39d348d1e0e33e6210942b4b9a4938e739059
bmarrao <73405469+bmarrao@users.noreply.github.com>
2022-12-21 22:18:48 +0000

    Update queries2.sql

--- amazoniabd/queries2.sql
+++ amazoniabd/queries2.sql
@@ -28,10 +28,10 @@ delimiter ;
 #Breno
 
 #Falta group by e order by
-#π (sum (p.total) ( (ρ p (Pedido) ⨝p.id_pedido=t.fk_pedido ρ t (tem) ⨝t.fk_Funcionario=f.Funcionario_ID ρ f (Funcionario) ⨝f.Armazem_Funcinario=a.Armazem_ID ρ a (Armazem))
-Select sum(p.total) From Pedido p INNER JOIN tem t ON p.id_pedido=t.fk_pedido INNER JOIN Funcionario f ON t.fk_Funcionario=f.Funcionario_ID INNER JOIN Armazem a ON f.Armazem_Funcinario=a.Armazem_ID
+#τ a.Armazem_ID desc (π sum (p.total) a.Armazem_ID ( (ρ p (Pedido) ⨝p.id_pedido=t.fk_pedido ρ t (tem) ⨝t.fk_Funcionario=f.Funcionario_ID ρ f (Funcionario) ⨝f.Armazem_Funcinario=a.Armazem_ID ρ a (Armazem))
+Select sum(p.total), a.Armazem_ID From Pedido p INNER JOIN tem t ON p.id_pedido=t.fk_pedido INNER JOIN Funcionario f ON t.fk_Funcionario=f.Funcionario_ID INNER JOIN Armazem a ON f.Armazem_Funcinario=a.Armazem_ID
 GROUP BY a.Armazem_ID 
-ORDER BY a.Armazem_ID
+ORDER BY a.Armazem_ID Desc
 
 
 #πfk_Pedido_ID_Pedido (σ fk_Funcionario_Funcionario_ID = id_func (tem)) com qualquer id_func, essa procedure diz a quais pedidos um funcionario esta relacionado
