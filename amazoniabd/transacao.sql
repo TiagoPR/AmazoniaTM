94e887007c560aa90698d6a163a0bca3d3e991c5
Tiago <tiagopassosrodrigues@hotmail.com>
2022-12-22 13:19:10 +0000

    Transacao

---
+++ amazoniabd/transacao.sql
@@ -0,0 +1,26 @@
+DELIMITER $$
+CREATE PROCEDURE AtualizaStock(IN item INT, IN pedido INT, IN Quantidade INT, IN IVA INT, IN Valor FLOAT)
+
+BEGIN 
+declare armazem int;
+declare vstock int;
+START TRANSACTION;
+-- 1ª Operação - INSERT 
+INSERT INTO contem
+    (fk_Item_Item_ID, fk_Pedido_ID_Pedido, Quantidade, Valor, IVA)
+    VALUES(item,pedido,Quantidade,IVA,Valor);
+
+set armazem = (select fk_Armazem_Armazem_ID from pedido where ID_Pedido = pedido);
+set vstock = (select stock from possui where fk_Item_Item_ID = pedido and fk_Armazem_Armazem_ID = armazem);
+IF vstock <= 0 then
+	rollback;
+end if;
+update possui
+    SET stock = vstock - Quantidade
+    WHERE fk_Item_Item_ID = item;
+        
+END$$
+DELIMITER ;
+
+select * from contem;
+select * from possui;
