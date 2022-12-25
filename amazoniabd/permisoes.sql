53de67cb5c64c073fff5b873a88c4062dd44d984
brenin <marraob@gmail.com>
2022-12-25 19:52:45 +0000

    fazendo permissoes

--- amazoniabd/permisoes.sql
+++ amazoniabd/permisoes.sql
@@ -1,6 +1,47 @@
 CREATE USER 'gestor'@'localhost';
+
 SET PASSWORD FOR 'gestor'@'localhost' = 'gestor1234';
 
 GRANT SELECT, INSERT ON Amazonia.funcionario TO 'gestor'@'localhost'; # RC01
 
-GRANT SELECT, INSERT ON Amazonia.armazem TO 'gestor'@'localhost'; # RC09
+GRANT UPDATE (Salario, Desempenho) 
+	ON Amazonia.funcionario TO 'gestor'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Amazonia.armazem TO 'gestor'@'localhost'; # RC09
+
+GRANT SELECT ON Amazonia.Pedido TO 'gestor'@'localhost'; # RC05
+
+GRANT UPDATE (Estado, Data_Entrega, Data_Pedido, Data_Envio) 
+	ON Amazonia.Pedido TO 'gestor'@'localhost';
+	
+GRANT SELECT, INSERT, UPDATE ON Amazonia.Item TO 'gestor'@'localhost'; # RC05
+
+GRANT SELECT, INSERT, UPDATE ON Amazonia.possui TO 'gestor'@'localhost'; # RC05
+
+--CREATE USER 'cliente'@'localhost';
+--#SET PASSWORD FOR 'cliente'@'localhost' = 'cliente1234';
+
+--GRANT SELECT, INSERT ON Amazonia.Cliente TO 'cliente'@'localhost'; # RC09
+
+--GRANT SELECT, INSERT ON Amazonia.contem TO 'cliente'@'localhost'; # RC09
+
+--GRANT SELECT, INSERT ON Amazonia.Pedido TO 'cliente'@'localhost'; # RC09
+
+CREATE USER 'JeffersonBazos'@'localhost';
+
+SET PASSWORD FOR 'JeffersonBazos'@'localhost' = 'JeffersonBazos1234';
+
+GRANT ALL ON *.* TO 'JeffersonBazos'@'localhost';
+
+CREATE USER 'funcionario'@'localhost';
+
+SET PASSWORD FOR 'funcionario'@'localhost' = 'funcionario1234';
+
+GRANT UPDATE (Endereco) 
+	ON Amazonia.funcionario TO 'funcionario'@'localhost';
+	
+CREATE USER 'distribuidor'@'localhost';
+
+SET PASSWORD FOR 'distribuidor'@'localhost' = 'distribuidor1234';
+
+GRANT UPDATE (Estado, Data_Entrega, Data_Pedido, Data_Envio) 
+	ON Amazonia.Pedido TO 'distribuidor'@'localhost';
